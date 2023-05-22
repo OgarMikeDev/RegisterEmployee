55c32e15532c065c4931541eb4be548f5c0d0638
Mike Ogar <ogar.m.v@mail.ru>
2023-05-22 06:44:26 +0300

    "add employee"

---
+++ src/main/java/com/example/demo/services/LoginServlet.java
@@ -0,0 +1,8 @@
+package com.example.demo.services;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "login-servlet")
+public class LoginServlet extends HttpServlet {
+}
